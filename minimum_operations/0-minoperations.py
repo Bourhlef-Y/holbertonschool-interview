8cb351900b9c29e280119273cac27e450af8dff0
Bourhlef-Y <bourhlef.yacine53@gmail.com>
2025-01-24 13:38:41 +0100

    d

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,38 +1,23 @@
 #!/usr/bin/python3
 """
-Module that calculates minimum operations to achieve n H characters
+This module defines the function minOperations.
 """
 
 
 def minOperations(n):
     """
-    Calculates the fewest number of operations needed to result in exactly n H
-    characters in a file that can only be manipulated by Copy All and Paste
-    operations.
-
-    Args:
-        n: Target number of H characters
-
-    Returns:
-        Integer representing minimum number of operations needed
-        Returns 0 if n is impossible to achieve
+    Calculate the minimum number of operations to result in exactly n H charac.
     """
-    if n <= 1:
+    if n < 2:
         return 0
 
     operations = 0
-    divisor = 2
+    factor = 2
 
     while n > 1:
-        while n % divisor == 0:
-            operations += divisor
-            n = n // divisor
-        divisor += 1
-
-        # If divisor squared is greater than n, n is prime
-        if divisor * divisor > n:
-            if n > 1:
-                operations += n
-            break
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
 
     return operations
