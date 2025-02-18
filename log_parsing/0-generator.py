643eb759fff3047cdecfdda40319f725de35cb68
Bourhlef-Y <bourhlef.yacine53@gmail.com>
2025-02-18 15:31:37 +0100

    add files

--- log_parsing/0-generator.py
+++ log_parsing/0-generator.py
@@ -6,15 +6,10 @@ import datetime
 
 for i in range(10000):
     sleep(random.random())
-    ip = "{:d}.{:d}.{:d}.{:d}".format(
-        random.randint(1, 255),
-        random.randint(1, 255),
-        random.randint(1, 255),
-        random.randint(1, 255)
-    )
-    status = random.choice([200, 301, 400, 401, 403, 404, 405, 500])
-    size = random.randint(1, 1024)
-
-    log_line = f"{ip} - [{datetime.datetime.now()}] \"GET /projects/260 HTTP/1.1\" {status} {size}\n"
-    sys.stdout.write(log_line)
+    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
+        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
+        datetime.datetime.now(),
+        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+        random.randint(1, 1024)
+    ))
     sys.stdout.flush()
