2651a4df3110580e7811ab3b12a0e5acfd0532fd
Bourhlef-Y <bourhlef.yacine53@gmail.com>
2025-03-21 09:28:24 +0100

    Refactor slide_line.c for consistent formatting [Betty Style requirements was not respected, is it now]
    Updated the formatting of the slide_line.c file to ensure consistent indentation and spacing throughout the merge and slide functions. This improves code readability without altering functionality.

--- slide_line/0-slide_line.c
+++ slide_line/0-slide_line.c
@@ -7,16 +7,16 @@
  */
 static void merge_left(int *line, size_t size)
 {
-    size_t i;
-    
-    for (i = 0; i < size - 1; i++)
-    {
-        if (line[i] != 0 && line[i] == line[i + 1])
-        {
-            line[i] *= 2;
-            line[i + 1] = 0;
-        }
-    }
+	size_t i;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		if (line[i] != 0 && line[i] == line[i + 1])
+		{
+			line[i] *= 2;
+			line[i + 1] = 0;
+		}
+	}
 }
 
 /**
@@ -26,16 +26,16 @@ static void merge_left(int *line, size_t size)
  */
 static void merge_right(int *line, size_t size)
 {
-    size_t i;
-    
-    for (i = size - 1; i > 0; i--)
-    {
-        if (line[i] != 0 && line[i] == line[i - 1])
-        {
-            line[i] *= 2;
-            line[i - 1] = 0;
-        }
-    }
+	size_t i;
+
+	for (i = size - 1; i > 0; i--)
+	{
+		if (line[i] != 0 && line[i] == line[i - 1])
+		{
+			line[i] *= 2;
+			line[i - 1] = 0;
+		}
+	}
 }
 
 /**
@@ -45,20 +45,20 @@ static void merge_right(int *line, size_t size)
  */
 static void slide_left(int *line, size_t size)
 {
-    size_t i, pos = 0;
-    
-    for (i = 0; i < size; i++)
-    {
-        if (line[i] != 0)
-        {
-            if (i != pos)
-            {
-                line[pos] = line[i];
-                line[i] = 0;
-            }
-            pos++;
-        }
-    }
+	size_t i, pos = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		if (line[i] != 0)
+		{
+			if (i != pos)
+			{
+				line[pos] = line[i];
+				line[i] = 0;
+			}
+			pos++;
+		}
+	}
 }
 
 /**
@@ -68,21 +68,21 @@ static void slide_left(int *line, size_t size)
  */
 static void slide_right(int *line, size_t size)
 {
-    size_t i;
-    int pos = size - 1;
-    
-    for (i = size - 1; i < size; i--)
-    {
-        if (line[i] != 0)
-        {
-            if (i != (size_t)pos)
-            {
-                line[pos] = line[i];
-                line[i] = 0;
-            }
-            pos--;
-        }
-    }
+	size_t i;
+	int pos = size - 1;
+
+	for (i = size - 1; i < size; i--)
+	{
+		if (line[i] != 0)
+		{
+			if (i != (size_t)pos)
+			{
+				line[pos] = line[i];
+				line[i] = 0;
+			}
+			pos--;
+		}
+	}
 }
 
 /**
@@ -94,21 +94,21 @@ static void slide_right(int *line, size_t size)
  */
 int slide_line(int *line, size_t size, int direction)
 {
-    if (!line || (direction != SLIDE_LEFT && direction != SLIDE_RIGHT))
-        return (0);
+	if (!line || (direction != SLIDE_LEFT && direction != SLIDE_RIGHT))
+		return (0);
 
-    if (direction == SLIDE_LEFT)
-    {
-        slide_left(line, size);
-        merge_left(line, size);
-        slide_left(line, size);
-    }
-    else
-    {
-        slide_right(line, size);
-        merge_right(line, size);
-        slide_right(line, size);
-    }
+	if (direction == SLIDE_LEFT)
+	{
+		slide_left(line, size);
+		merge_left(line, size);
+		slide_left(line, size);
+	}
+	else
+	{
+		slide_right(line, size);
+		merge_right(line, size);
+		slide_right(line, size);
+	}
 
-    return (1);
+	return (1);
 }
