1db46ed87185a2ddf933bf4b9b6b9098e5d022e9
Bourhlef-Y <bourhlef.yacine53@gmail.com>
2025-06-04 14:06:04 +0200

    add Rotate 2D Matrix

---
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module pour faire pivoter une matrice 2D de 90 degrés dans le sens horaire"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    Fait pivoter une matrice 2D de n x n de 90 degrés dans le sens horaire
+    Args:
+        matrix: matrice 2D à faire pivoter
+    """
+    n = len(matrix)
+    
+    # Première étape : transposer la matrice
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    
+    # Deuxième étape : inverser chaque ligne
+    for i in range(n):
+        matrix[i].reverse() 
