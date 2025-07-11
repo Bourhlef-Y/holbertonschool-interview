51963faa975a0891adf3e4dca04b2ea2a2c87fdd
Bourhlef-Y <bourhlef.yacine53@gmail.com>
2025-07-11 12:13:31 +0200

    PEP8 coorction

--- double_circular_linked_list/0-add_node.c
+++ double_circular_linked_list/0-add_node.c
@@ -10,28 +10,28 @@
  */
 List *create_node(char *str)
 {
-    List *new_node;
-    char *new_str;
+	List *new_node;
+	char *new_str;
 
-    if (!str)
-        return (NULL);
+	if (!str)
+		return (NULL);
 
-    new_node = malloc(sizeof(List));
-    if (!new_node)
-        return (NULL);
+	new_node = malloc(sizeof(List));
+	if (!new_node)
+		return (NULL);
 
-    new_str = strdup(str);
-    if (!new_str)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	new_str = strdup(str);
+	if (!new_str)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    new_node->str = new_str;
-    new_node->prev = NULL;
-    new_node->next = NULL;
+	new_node->str = new_str;
+	new_node->prev = NULL;
+	new_node->next = NULL;
 
-    return (new_node);
+	return (new_node);
 }
 
 /**
@@ -43,34 +43,35 @@ List *create_node(char *str)
  */
 List *add_node_end(List **list, char *str)
 {
-    List *new_node, *last;
+	List *new_node, *last;
 
-    if (!list)
-        return (NULL);
+	if (!list)
+		return (NULL);
 
-    new_node = create_node(str);
-    if (!new_node)
-        return (NULL);
+	new_node = create_node(str);
+	if (!new_node)
+		return (NULL);
 
-    if (!*list)
-    {
-        *list = new_node;
-        new_node->next = new_node;
-        new_node->prev = new_node;
-        return (new_node);
-    }
+	if (!*list)
+	{
+		*list = new_node;
+		new_node->next = new_node;
+		new_node->prev = new_node;
+		return (new_node);
+	}
 
-    last = (*list)->prev;
-    new_node->next = *list;
-    new_node->prev = last;
-    last->next = new_node;
-    (*list)->prev = new_node;
+	last = (*list)->prev;
+	new_node->next = *list;
+	new_node->prev = last;
+	last->next = new_node;
+	(*list)->prev = new_node;
 
-    return (new_node);
+	return (new_node);
 }
 
 /**
- * add_node_begin - Adds a new node to the beginning of a double circular linked list
+ * add_node_begin - Adds a new node to the beginning
+ * of a double circular linked list
  * @list: pointer to the list to modify
  * @str: string to copy into the new node
  *
@@ -78,14 +79,14 @@ List *add_node_end(List **list, char *str)
  */
 List *add_node_begin(List **list, char *str)
 {
-    List *new_node;
+	List *new_node;
 
-    new_node = add_node_end(list, str);
-    if (!new_node)
-        return (NULL);
+	new_node = add_node_end(list, str);
+	if (!new_node)
+		return (NULL);
 
-    if (*list != new_node)
-        *list = new_node;
+	if (*list != new_node)
+		*list = new_node;
 
-    return (new_node);
+	return (new_node);
 }
